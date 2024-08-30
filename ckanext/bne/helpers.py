156a6f287524d4ddbc13a3db856b1116ef4ec327
Ernesto Hontecillas Molina <ehonteci@tragsa.es>
2024-08-30 15:19:28 +0200

    large general changes fixed css in small-screen devices Centered and aligned elements fixed broken header fixed broken footer fixed showcase rendering and made it more similar to https://bnelab.bne.es/ swapped several lists with nested <div> deleted unused helpers added comments to helpers taken from ckanext-surrey

--- ckanext/bne/helpers.py
+++ ckanext/bne/helpers.py
@@ -2,6 +2,7 @@ import logging
 
 import ckan.lib.helpers as h
 import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 
 import ckanext.bne.config as bne_config
 
@@ -31,3 +32,54 @@ def bne_get_bne_base_url():
         str: The URL.
     """
     return bne_config.bne_base_url
+
+## Helpers out of ckanext-surrey
+# TODO test every helper to check if they work correctly in the newer ckan/python version 
+
+
+@helper
+def get_group_list():
+    """
+    Retreives the group list 
+
+    returns:
+        list: List of groups
+    """
+    groups = tk.get_action('group_list')(
+        data_dict={'all_fields': True})
+
+    return groups
+
+
+@helper
+def get_summary_list(num_packages):    
+    """
+    Retreives a summary of n packages ordered by recently added
+
+    returns:
+        list: List of packages
+    """
+    list_without_summary = \
+    tk.get_action('package_search')(data_dict={'rows': num_packages, 'sort': 'metadata_modified desc'})['results']
+    list_with_summary = []
+    for package in list_without_summary:
+        list_with_summary.append(tk.get_action('package_show')(
+            data_dict={'id': package['name'], 'include_tracking': True})
+        )
+    return list_with_summary
+
+@helper
+def get_visit_summary_list(num_packages):
+    """
+    Retreives a summary of packages ordered by times visited
+
+    returns:
+        list: List of packages
+    """
+    list_without_summary = tk.get_action('package_search')(data_dict={'rows':num_packages,'sort':'views_recent desc'})['results']
+    list_with_summary = []
+    for package in list_without_summary:
+        list_with_summary.append(tk.get_action('package_show')(
+        data_dict={'id':package['name'],'include_tracking':True})
+        )
+    return list_with_summary
