37d7666f7ad20a5cba030676f42e3b3a898cdedb
Ernesto Hontecillas Molina <ehonteci@tragsa.es>
2024-10-11 17:42:34 +0200

    Added independent html for footer info links Now these links aren't dependant on ckanext-pages

--- ckanext/bne/blueprint.py
+++ ckanext/bne/blueprint.py
@@ -9,8 +9,19 @@ logger = getLogger(__name__)
 get_action = logic.get_action
 
 bne = Blueprint(u'bne', __name__)
-
 def base():
     return render_template('bne/base.html')
 
+def api_info():
+    return render_template('bne/api_info.html')
+
+def informacion():
+    return render_template('bne/informacion.html')
+
+def resumen():
+    return render_template('bne/resumen.html')
+
 bne.add_url_rule('/bne/', view_func=base)
+bne.add_url_rule('/bne/api_info/', view_func=api_info)
+bne.add_url_rule('/bne/informacion/', view_func=informacion)
+bne.add_url_rule('/bne/resumen/', view_func=resumen)
